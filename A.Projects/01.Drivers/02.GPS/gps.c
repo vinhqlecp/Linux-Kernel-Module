2b941fdba5478e5c5b8a24ae4f4321f88351864c
vinhqle.cp <vinhqle.cp@gmail.com>
2023-07-31 16:35:09 +0700

    Update source code GPS driver

--- A.Projects/01.Drivers/02.GPS/gps.c
+++ A.Projects/01.Drivers/02.GPS/gps.c
@@ -10,33 +10,143 @@
 #define DRIVER_CLASS "rpi4_gps"
 
 #define IRQ_PIN 23
-#define IRQ_PIN_NAME "rpi-gpio-23"
+#define IRQ_PIN_NAME "rpi4-gpio-23"
 
 const char driver_name[18] = "U-Blox GPS driver";
 
-unsigned int irq_number;
+/* Buffer for data */
+static char buffer[1024];
+static int buffer_pointer = 0;
+
+static unsigned int irq_number;
+
+struct _vchar_drv {
+	dev_t dev_num;
+	struct class *dev_class;
+	struct device *dev;
+	struct cdev *vcdev;
+	unsigned int open_cnt;
+} vchar_drv;
+
+/**
+ * @brief Read data out of the buffer
+ */
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	/* Get amount of data to copy */
+	to_copy = min(count, buffer_pointer);
+
+	/* Copy data to user */
+	not_copied = copy_to_user(user_buffer, buffer, to_copy);
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief Write data to buffer
+ */
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	/* Get amount of data to copy */
+	to_copy = min(count, sizeof(buffer));
+
+	/* Copy data to user */
+	not_copied = copy_from_user(buffer, user_buffer, to_copy);
+	buffer_pointer = to_copy;
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+static long int driver_ioctl(struct file *file, unsigned cmd, unsigned long arg){
+	return 0;
+}
+
+/**
+ * @brief This function is called, when the device file is closed
+ */
+static int driver_open(struct inode *device_file, struct file *instance) {
+	vchar_drv.open_cnt++;
+	return 0;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int driver_close(struct inode *device_file, struct file *instance) {
+	return 0;
+}
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
+	.write = driver_write,
+	.unlocked_ioctl = driver_ioctl
+};
 
 /**
  * @brief Interrupt service routine is called, when interrupt is triggered
  */
 static irq_handler_t gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs) {
-	printk("gpio_irq: Interrupt was triggered and ISR was called!\n");
+
 	return (irq_handler_t) IRQ_HANDLED; 
 }
 
 /**
  * @brief This function is called, when the module is loaded into the kernel
  */
-static int __init gps_driver_init(void) {
+static int __init symple_module_init(void) {
+	int ret = -1;
+
+	ret = alloc_chrdev_region(&vchar_drv.dev_num, 0, 1, DRIVER_NAME);
+	if(ret < 0) {
+		printk(KERN_ERR "Failed to register device number dynamically.\n");
+		goto failed_register_devnum;
+	}
+	printk(KERN_INFO "Allocated device number with major=%d and minor=%d.\n", MAJOR(vchar_drv.dev_num), MINOR(vchar_drv.dev_num));
+
+	vchar_drv.dev_class = class_create(THIS_MODULE, DRIVER_CLASS);
+	if(vchar_drv.dev_class == NULL) {
+		printk(KERN_ERR "Failed to create class.\n");
+		goto failed_create_class;
+	}
+
+	vchar_drv.dev = device_create(vchar_drv.dev_class, NULL, vchar_drv.dev_num, NULL, DRIVER_NAME);
+	if(IS_ERR(vchar_drv.dev)) {
+		printk(KERN_ERR "Failed to create a device.\n");
+		goto failed_create_device;
+	}
+
+	vchar_drv.vcdev = cdev_alloc();
+	if(vchar_drv.vcdev == NULL) {
+		printk(KERN_ERR "Failed to allocate cdev structure.\n");
+		goto failed_allocate_cdev;
+	}
+	cdev_init(vchar_drv.vcdev, &fops);
+	ret = cdev_add(vchar_drv.vcdev, vchar_drv.dev_num, 1);
+	if(ret < 0) {
+		printk(KERN_ERR "Failed to add character device to the system.\n");
+		goto failed_allocate_cdev;
+	}
+
 	/* Setup the gpio */
 	if(gpio_request(IRQ_PIN, IRQ_PIN_NAME)) {
-		printk("Error!\nCan not allocate GPIO %d\n", IRQ_PIN);
+		printk(KERN_ERR "Can not allocate GPIO %d\n", IRQ_PIN);
 		return -1;
 	}
 
-	/* Set GPIO 23 direction */
+	/* Set GPIO direction */
 	if(gpio_direction_input(IRQ_PIN)) {
-		printk("Error!\nCan not set GPIO %d to input!\n", IRQ_PIN);
+		printk(KERN_ERR "Can not set GPIO %d to input!\n", IRQ_PIN);
 		gpio_free(IRQ_PIN);
 		return -1;
 	}
@@ -45,28 +155,40 @@ static int __init gps_driver_init(void) {
 
 	/* Setup the interrupt */
 	irq_number = gpio_to_irq(IRQ_PIN);
-
-	if(request_irq(irq_number, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_RISING, "my_gpio_irq", NULL) != 0){
-		printk("Error!\nCan not request interrupt nr.: %d\n", irq_number);
+	if(request_irq(irq_number, (irq_handler_t) gpio_irq_handler, IRQF_TRIGGER_RISING, "gps_gpio_irq", NULL) != 0){
+		printk(KERN_ERR "Can not request interrupt nr.: %d\n", irq_number);
 		gpio_free(IRQ_PIN);
 		return -1;
 	}
 	printk("GPIO %d is mapped to IRQ Nr.: %d\n", IRQ_PIN, irq_number);
-
 	return 0;
+
+failed_allocate_cdev:
+failed_create_device:
+	class_destroy(vchar_drv.dev_class);
+failed_create_class:
+	unregister_chrdev_region(vchar_drv.dev_num, 1);
+failed_register_devnum:
+	return ret;
 }
 
 /**
  * @brief This function is called, when the module is removed from the kernel
  */
-static void __exit gps_driver_exit(void) {
+static void __exit simple_module_exit(void) {
+	// Release GPS IRQ pin
 	free_irq(irq_number, NULL);
 	gpio_free(IRQ_PIN);
+
+	cdev_del(vchar_drv.vcdev);
+	device_destroy(vchar_drv.dev_class, vchar_drv.dev_num);
+	class_destroy(vchar_drv.dev_class);
+	unregister_chrdev_region(vchar_drv.dev_num, 1);
 	printk(KERN_INFO "Exit %s\n", driver_name);
 }
 
-module_init(gps_driver_init);
-module_exit(gps_driver_exit);
+module_init(symple_module_init);
+module_exit(simple_module_exit);
 
 /* Meta Information */
 MODULE_LICENSE(DRIVER_LICENSE);
--- A.Projects/01.Drivers/02.GPS/gps.h
+++ A.Projects/01.Drivers/02.GPS/gps.h
@@ -3,6 +3,11 @@
 
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/ioctl.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 
